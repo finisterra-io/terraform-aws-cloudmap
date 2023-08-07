4997fdbe1e8e84c6271bef82e90350ab8e3ca5c6
xneyder <xneyder@gmail.com>
2023-08-06 21:11:06 -0400

    Add files

--- variables.tf
+++ variables.tf
@@ -12,3 +12,15 @@ variable "service_names" {
   description = "The names of the services to create"
   type        = map(any)
 }
+
+variable "create_private_dns_namespace" {
+  description = "Whether to create a private DNS namespace"
+  type        = bool
+  default     = true
+}
+
+variable "tags" {
+  description = "The tags to associate with the namespace"
+  type        = map(string)
+  default     = {}
+}
